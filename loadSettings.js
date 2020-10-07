e4f084d0b37f6518db49aeca75a85da6755b127a
JacobEckroth <jacobeckroth@gmail.com>
2020-10-07 16:35:34 -0700

    cool shit

--- loadSettings.js
+++ loadSettings.js
@@ -4,4 +4,19 @@ window.addEventListener("load",removePreload);
 function removePreload(){
 
     document.querySelector(".preload").classList.remove("preload")
+    appearText();
+}
+
+
+
+
+var currentUpdating = 0
+function appearText(){
+ 
+    var textHolder = document.getElementById("navList")
+    textHolder.children[currentUpdating].style.fontSize = "12vh";
+    currentUpdating+=1
+    if(!(currentUpdating>= textHolder.children.length)){
+        setTimeout(appearText,500);
+    }
 }
