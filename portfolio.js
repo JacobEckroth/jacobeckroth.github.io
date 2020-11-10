a1cb36a29fdba7dfb8780ea4e30b03e710d1f3cc
JacobEckroth <jacobeckroth@gmail.com>
2020-11-09 16:26:21 -0800

    yay

---
+++ portfolio.js
@@ -0,0 +1,5 @@
+var twenty = document.getElementById("twentyfortyeight");
+twenty.addEventListener("click",function(){
+    location.href="projects/2048plus.html";
+    console.log("we there yo");
+})
